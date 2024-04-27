5bbddbdde3078bf1cd73bfde9aadfb176b3fd2d7
lauravel9 <148814865+lauravel9@users.noreply.github.com>
2024-04-26 21:56:05 -0500

    ahorasi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,9 +45,8 @@ public class HomeController : Controller
         string turno = "";
         var result = await _context.Categorias.FirstOrDefaultAsync(c => c.Siglas == siglas);
         int contador = result.Contador +1;
-
         turno = siglas+"-"+(contador < 10 ? "00"+contador: "0"+contador);
-
+    //hola
         ViewData["turno"] = turno;
         var tipo = HttpContext.Request.Cookies["tipo"];
         ViewData["tipo"]=tipo;
